e1f7ba19c7c7d9290ff3f2a60df515d15986ac7b
Michael Christenson II <christensonmi.ctr@socom.mil>
2025-07-10 07:19:03 -0400

    Use PathValue

--- api/users/handler.go
+++ api/users/handler.go
@@ -2,7 +2,6 @@ package users
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 )
 
@@ -19,7 +18,6 @@ func (h Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Route not found")
 	case http.MethodGet:
 		userId := r.PathValue("userId")
-		log.Printf("userId: %s", userId)
 		if userId == "" {
 			w.WriteHeader(404)
 			fmt.Fprintf(w, "Route not found")
@@ -35,7 +33,7 @@ func (h Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		}
 		HandleRegister(w, r)
 	case http.MethodDelete:
-		userId := r.URL.Query().Get("userId")
+		userId := r.PathValue("userId")
 		if userId != "" {
 			w.WriteHeader(404)
 			fmt.Fprintf(w, "Route not found")
