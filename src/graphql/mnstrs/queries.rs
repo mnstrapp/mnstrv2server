7d2078f11b13d10e2287aaaa7b7290499911337f
Michael Christenson II <m3talsmith@gmail.com>
2025-09-14 16:51:48 -0400

    Added find mnstr by qrcode

--- src/graphql/mnstrs/queries.rs
+++ src/graphql/mnstrs/queries.rs
@@ -5,12 +5,16 @@ pub struct MnstrQueryType;
 
 #[juniper::graphql_object]
 impl MnstrQueryType {
-    async fn mnstrs(ctx: &Ctx) -> Result<Vec<Mnstr>, FieldError> {
-        mnstrs(ctx).await
+    async fn list(ctx: &Ctx) -> Result<Vec<Mnstr>, FieldError> {
+        list(ctx).await
+    }
+
+    async fn qr_code(ctx: &Ctx, qr_code: String) -> Result<Option<Mnstr>, FieldError> {
+        by_qr_code(ctx, qr_code).await
     }
 }
 
-async fn mnstrs(ctx: &Ctx) -> Result<Vec<Mnstr>, FieldError> {
+async fn list(ctx: &Ctx) -> Result<Vec<Mnstr>, FieldError> {
     if let None = ctx.session {
         return Err(FieldError::from("Invalid session"));
     }
@@ -21,6 +25,26 @@ async fn mnstrs(ctx: &Ctx) -> Result<Vec<Mnstr>, FieldError> {
         Err(e) => {
             println!("[mnstrs] Failed to get mnstrs: {:?}", e);
             return Ok(vec![]);
+        }
+    }
+}
+
+async fn by_qr_code(ctx: &Ctx, qr_code: String) -> Result<Option<Mnstr>, FieldError> {
+    if let None = ctx.session {
+        return Err(FieldError::from("Invalid session"));
+    }
+    let session = ctx.session.as_ref().unwrap().clone();
+
+    let params = vec![
+        ("user_id", session.user_id.clone().into()),
+        ("mnstr_qr_code", qr_code.clone().into()),
+    ];
+
+    match Mnstr::find_one_by(params).await {
+        Ok(mnstr) => Ok(Some(mnstr)),
+        Err(e) => {
+            println!("[get_by_qr_code] Failed to get mnstr: {:?}", e);
+            return Ok(None);
         }
     }
 }
