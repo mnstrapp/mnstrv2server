7352784e6d848bf625cef6aff3e39670df1f1575
Michael Christenson II <m3talsmith@gmail.com>
2025-10-10 17:18:33 -0400

    Add full mnstr create

--- src/graphql/mnstrs/mutations.rs
+++ src/graphql/mnstrs/mutations.rs
@@ -9,6 +9,44 @@ impl MnstrMutationType {
     async fn collect(ctx: &Ctx, mnstr_qr_code: String) -> Result<Mnstr, FieldError> {
         collect(ctx, mnstr_qr_code).await
     }
+    async fn create(
+        ctx: &Ctx,
+        mnstr_name: Option<String>,
+        mnstr_description: Option<String>,
+        mnstr_qr_code: Option<String>,
+        current_health: Option<i32>,
+        max_health: Option<i32>,
+        current_attack: Option<i32>,
+        max_attack: Option<i32>,
+        current_defense: Option<i32>,
+        max_defense: Option<i32>,
+        current_speed: Option<i32>,
+        max_speed: Option<i32>,
+        current_intelligence: Option<i32>,
+        max_intelligence: Option<i32>,
+        current_magic: Option<i32>,
+        max_magic: Option<i32>,
+    ) -> Result<Mnstr, FieldError> {
+        create(
+            ctx,
+            mnstr_name,
+            mnstr_description,
+            mnstr_qr_code,
+            current_health,
+            max_health,
+            current_attack,
+            max_attack,
+            current_defense,
+            max_defense,
+            current_speed,
+            max_speed,
+            current_intelligence,
+            max_intelligence,
+            current_magic,
+            max_magic,
+        )
+        .await
+    }
     async fn update(
         ctx: &Ctx,
         id: String,
@@ -63,6 +101,57 @@ pub async fn collect(ctx: &Ctx, mnstr_qr_code: String) -> Result<Mnstr, FieldErr
         String::new(),
         mnstr_qr_code.clone(),
     );
+
+    if let Some(error) = mnstr.create().await {
+        println!("[collect] Failed to create mnstr: {:?}", error);
+        return Err(FieldError::from("Failed to create mnstr"));
+    }
+
+    Ok(mnstr)
+}
+
+pub async fn create(
+    ctx: &Ctx,
+    mnstr_name: Option<String>,
+    mnstr_description: Option<String>,
+    mnstr_qr_code: Option<String>,
+    current_health: Option<i32>,
+    max_health: Option<i32>,
+    current_attack: Option<i32>,
+    max_attack: Option<i32>,
+    current_defense: Option<i32>,
+    max_defense: Option<i32>,
+    current_speed: Option<i32>,
+    max_speed: Option<i32>,
+    current_intelligence: Option<i32>,
+    max_intelligence: Option<i32>,
+    current_magic: Option<i32>,
+    max_magic: Option<i32>,
+) -> Result<Mnstr, FieldError> {
+    if let None = ctx.session {
+        return Err(FieldError::from("Invalid session"));
+    }
+    let session = ctx.session.as_ref().unwrap().clone();
+
+    let mut mnstr = Mnstr::new(
+        session.user_id.clone(),
+        mnstr_name.unwrap_or(String::new()),
+        mnstr_description.unwrap_or(String::new()),
+        mnstr_qr_code.unwrap_or(String::new()),
+    );
+
+    mnstr.current_health = current_health.unwrap_or(10) as i32;
+    mnstr.max_health = max_health.unwrap_or(10) as i32;
+    mnstr.current_attack = current_attack.unwrap_or(10) as i32;
+    mnstr.max_attack = max_attack.unwrap_or(10) as i32;
+    mnstr.current_defense = current_defense.unwrap_or(10) as i32;
+    mnstr.max_defense = max_defense.unwrap_or(10) as i32;
+    mnstr.current_speed = current_speed.unwrap_or(10) as i32;
+    mnstr.max_speed = max_speed.unwrap_or(10) as i32;
+    mnstr.current_intelligence = current_intelligence.unwrap_or(10) as i32;
+    mnstr.max_intelligence = max_intelligence.unwrap_or(10) as i32;
+    mnstr.current_magic = current_magic.unwrap_or(10) as i32;
+    mnstr.max_magic = max_magic.unwrap_or(10) as i32;
 
     if let Some(error) = mnstr.create().await {
         println!("[collect] Failed to create mnstr: {:?}", error);
