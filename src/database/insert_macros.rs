a782ebb49e14f96cdb71f61e2b0af1de3a57ed11
Michael Christenson II <m3talsmith@gmail.com>
2026-05-20 14:27:36 -0400

    Removed debug statement

--- src/database/insert_macros.rs
+++ src/database/insert_macros.rs
@@ -381,8 +381,8 @@ macro_rules! insert_resource_batch {
                 }
                 query.push_str(&value_query);
             }
+
             query.push_str(" RETURNING *");
-            println!("{}", query);
 
             let mut query = sqlx::query(&query);
             for (_, value) in values.iter().enumerate() {
